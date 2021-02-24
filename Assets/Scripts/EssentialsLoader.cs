eb8363151ddd16bdf7f59acca4a8d225aa0c0d9e
Epic Dog Games <epicdoggames@gmail.com>
2021-02-24 10:44:05 -0600

    2D rpg game. Udemy course. Content includes everything up until the Dialogue Section.

---
+++ Assets/Scripts/EssentialsLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EssentialsLoader : MonoBehaviour
+{
+    public GameObject uiScreen;
+    public GameObject player;
+    public GameObject gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (UIFade.instance == null)
+        {
+            UIFade.instance = Instantiate(uiScreen).GetComponent<UIFade>();
+        }
+
+        if (PlayerController.instance == null)
+        {
+            PlayerController.instance = Instantiate(player).GetComponent<PlayerController>();
+        }
+
+        if (GameManager.instance == null)
+        {
+            Instantiate(gameManager);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
