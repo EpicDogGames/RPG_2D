73544d06cb216df3dcc8b04fb1cb7e0ecbde3ad8
Epic Dog Games <37165313+EpicDogGames@users.noreply.github.com>
2021-03-12 12:37:45 -0600

    Completed main menu section

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string newGameScene;
+    public string loadingScene;
+
+    public GameObject continueButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       if (PlayerPrefs.HasKey("Current_Scene"))
+       {
+           continueButton.SetActive(true);
+       } 
+       else
+       {
+           continueButton.SetActive(false);
+       }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Continue()
+    {
+        SceneManager.LoadScene(loadingScene);
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(newGameScene);
+    }
+
+    public void Exit() 
+    {
+        Application.Quit();
+    }
+}
